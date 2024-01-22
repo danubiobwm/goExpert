9ae0ed1e94dad3efba5ee8ad4b9fc68154998f93
danubiobwm <danubio.bwm@gmail.com>
2024-01-22 10:14:12 -0300

    implementando getRepository
    Signed-off-by: danubiobwm <danubio.bwm@gmail.com>

--- uow/pkg/uow/uow.go
+++ uow/pkg/uow/uow.go
@@ -3,6 +3,8 @@ package uow
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 )
 
 type RepositoryFactory func(tx *sql.Tx) interface{}
@@ -10,7 +12,7 @@ type RepositoryFactory func(tx *sql.Tx) interface{}
 type UowInterface interface {
 	Register(name string, fc RepositoryFactory)
 	GetRepository(ctx context.Context, name string) (interface{}, error)
-	Do(ctx context.Context, fn func(uwo *UowInterface) error) error
+	Do(ctx context.Context, fn func(uwo *Uow) error) error
 	CommitOrRollback() error
 	Rollback() error
 	UnRegister(name string)
@@ -38,4 +40,72 @@ func (uow *Uow) Register(name string, fc RepositoryFactory) {
 
 func (uow *Uow) UnRegister(name string) {
 	delete(uow.Repositories, name)
+}
+
+func (uow *Uow) GetRepository(ctx context.Context, name string) (interface{}, error) {
+	if uow.Tx == nil {
+		tx, err := uow.Db.BeginTx(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		uow.Tx = tx
+	}
+	repo := uow.Repositories[name](uow.Tx)
+	return repo, nil
+}
+
+func (uow *Uow) Do(ctx context.Context, fn func(Uow *Uow) error) error {
+
+	if uow.Tx != nil {
+		return fmt.Errorf("transaction already started")
+	}
+
+	tx, err := uow.Db.BeginTx(ctx, nil)
+
+	if err != nil {
+		return err
+	}
+	uow.Tx = tx
+
+	err = fn(uow)
+
+	if err != nil {
+		errRb := uow.Rollback()
+
+		if errRb != nil {
+			return errors.New(fmt.Sprintf("original error: %s, rollback error: %s", err.Error(), errRb.Error()))
+		}
+
+		return err
+	}
+	return uow.CommitOrRollback()
+}
+
+func (u *Uow) Rollback() error {
+	if u.Tx != nil {
+		return errors.New("No transaction to rollback")
+	}
+	err := u.Tx.Rollback()
+	if err != nil {
+		return err
+	}
+	u.Tx = nil
+
+	return nil
+}
+
+func (uow *Uow) CommitOrRollback() error {
+	err := uow.Tx.Commit()
+
+	if err != nil {
+		errRb := uow.Rollback()
+
+		if errRb != nil {
+			return errors.New(fmt.Sprintf("original error: %s, rollback error: %s", err.Error(), errRb.Error()))
+		}
+
+		return err
+	}
+	uow.Tx = nil
+	return nil
 }
