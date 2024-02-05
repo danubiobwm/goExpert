066348cb171b850ebfc9252664fcee5444fb7fe1
danubiobwm <danubio.bwm@gmail.com>
2024-02-05 00:10:18 -0300

    clean arch desafio
    Signed-off-by: danubiobwm <danubio.bwm@gmail.com>

--- CleanArch/internal/infra/web/order_rest_handler.go
+++ CleanArch/internal/infra/web/order_rest_handler.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/devfullcycle/20-CleanArch/internal/usecase"
@@ -18,14 +19,17 @@ func NewOrderRestHandler(listOrdersUseCase *usecase.ListOrdersUseCase) *OrderRes
 }
 
 func (h *OrderRestHandler) ListOrdersHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handling ListOrders request")
 	orders, err := h.ListOrdersUseCase.Execute()
 	if err != nil {
+		log.Printf("Error listing orders: %s", err.Error())
 		http.Error(w, "Failed to list orders", http.StatusInternalServerError)
 		return
 	}
 
 	response, err := json.Marshal(orders)
 	if err != nil {
+		log.Printf("Error marshaling response: %s", err.Error())
 		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
 		return
 	}
--- CleanArch/internal/usecase/list_orders_usecase.go
+++ CleanArch/internal/usecase/list_orders_usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"log"
+
 	"github.com/devfullcycle/20-CleanArch/internal/entity"
 )
 
@@ -15,6 +17,7 @@ func NewListOrdersUseCase(OrderRepository entity.OrderRepositoryInterface) *List
 }
 
 func (uc *ListOrdersUseCase) Execute() ([]OrderOutputDTO, error) {
+	log.Println("Executing ListOrdersUseCase")
 	orders, err := uc.OrderRepository.List()
 	if err != nil {
 		return nil, err
